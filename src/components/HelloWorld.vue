c797fa536a4da6c4e8c89ac449a43b7d0f04b1b7
Carlos Gonzalez <carlosgj94@gmail.com>
2017-11-10 13:22:48 +0100

    Initial steps component in HelloWorld

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,48 @@
 <template>
-<div>
-  <el-button @click="visible = true">按钮</el-button>
-  <el-dialog :visible.sync="visible" title="Hello world">
-    <p>欢迎使用 Element</p>
-  </el-dialog>
-</div>
+    <el-row>
+        <el-col :span="12" :offset="6">
+            <el-steps :active="active" finish-status="success" simple>
+                <el-step title="Step 1"></el-step>
+                <el-step title="Step 2"></el-step>
+                <el-step title="Step 3"></el-step>
+            </el-steps>
+            <el-button style="margin-top: 12px;" @click="next">Next step</el-button>
+        </el-col>
+    </el-row>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    name: 'HelloWorld',
+    data() {
+        return {
+            active: 0
+        };
+    },
+
+    methods: {
+        next() {
+            if (this.active++ > 2) this.active = 0;
+        }
     }
-  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style >
+.el-button{
+    padding-top: 20px;
+    padding-bottom: 20px;
+    font-size: 25px;
+}
+.el-step__head{
+    font-size: 20px !important;
+}
+.el-step__title{
+    font-size: 20px !important;
+}
+.is-process{
+    font-size: 25px !important;
+}
 </style>
+
