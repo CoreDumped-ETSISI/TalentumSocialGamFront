04dff626e0209063cb1e5bd9324ed268ede4503b
Carlos Gonzalez <carlosgj94@gmail.com>
2017-11-10 11:51:10 +0100

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+  <el-button @click="visible = true">按钮</el-button>
+  <el-dialog :visible.sync="visible" title="Hello world">
+    <p>欢迎使用 Element</p>
+  </el-dialog>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
