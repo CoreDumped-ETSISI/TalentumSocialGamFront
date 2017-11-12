2f4056716d9de255297138dd57b9967d09304076
Néstor Javier Oubiña Arriaga <nestoroa@hotmail.com>
2017-11-12 23:04:00 +0100

    Font change

--- src/components/IntroComponents/Step5.vue
+++ src/components/IntroComponents/Step5.vue
@@ -4,7 +4,7 @@
             <el-card :body-style="{ padding: '0px' }">
                 <div style="padding: 14px;">
                     <h1>Termina el registro</h1>
-                    <el-form ref="form" :model="form" label-width="120px" label-position="top">
+                    <el-form ref="form" :model="form" label-position="top">
                         <el-form-item label="Teléfono">
                             <el-input type='tel' class="font-large text-centered" v-model="form.phone" v-on:input="change"></el-input>
                         </el-form-item>
